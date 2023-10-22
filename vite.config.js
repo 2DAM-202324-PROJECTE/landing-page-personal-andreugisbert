59d3bf8eb8dbb6fbf5864f75badfbe2470584123
agisbertb <agisbert@iesebre.com>
2023-10-22 18:59:59 +0200

    Canvi en l'estructura, afegida carpeta views amb les diferentes vistes i canvi de configuració al vite.config.js per fer la pàgina multipage

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
-export default{
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-    base: '/landing-page-personal-andreugisbert/',       
-}
+export default defineConfig({
+  base: "/landing-page-personal-andreugisbert/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        services: resolve(__dirname, "views/services.html"),
+        contact: resolve(__dirname, "views/contact.html"),
+        about: resolve(__dirname, "views/about.html"),
+      },
+    },
+  },
+});
