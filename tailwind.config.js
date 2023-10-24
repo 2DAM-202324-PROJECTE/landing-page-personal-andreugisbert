da519eec75af71d77a60b150fd14430e26337504
agisbertb <agisbert@iesebre.com>
2023-10-24 11:31:05 +0200

    Arreglat bug amb el dark mode, canvi de logo section i algun detallet més

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   darkMode: 'class',
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+    },
+    animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+    },
+    keyframes: {
+        'infinite-scroll': {
+            from: { transform: 'translateX(0)' },
+            to: { transform: 'translateX(-100%)' },
+        }
+    }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
   ],
 }
-
