16a38f71e1d0f3d926bd6afa4741d698c4835914
agisbertb <agisbert@iesebre.com>
2023-10-25 19:45:40 +0200

    Canvi de les funcions del dark mode per posar-les dins del main.js i fora dels .html

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-import Alpine from 'alpinejs'
- 
-window.Alpine = Alpine
- 
-Alpine.start()
+import Alpine from "alpinejs";
 
+window.Alpine = Alpine;
 
+Alpine.start();
+
+// Funció par establir el tema oscur o clar y actualitzar el logo
+function setTheme(theme) {
+  localStorage.theme = theme;
+  document.documentElement.classList.toggle("dark", theme === "dark");
+  const logoElement = document.getElementById("logo");
+  if (logoElement) {
+    logoElement.src =
+      theme === "dark" ? "images/LOGO_DARK.png" : "images/LOGO_LIGHT.png";
+  }
+}
+
+// Comprova si ja existeix una preferència a localStorage
+const storedTheme = localStorage.theme;
+
+// Si hi ha una preferència guardada, estableix el tema y actualitza el logo segons la preferència
+if (
+  storedTheme === "dark" ||
+  (!storedTheme && window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  setTheme("dark");
+} else {
+  setTheme("light");
+}
+
+// Agrega l'event click al botó per alternar entre els modes
+document.getElementById("theme-toggle").addEventListener("click", () => {
+  const isDarkMode = document.documentElement.classList.contains("dark");
+  setTheme(isDarkMode ? "light" : "dark");
+});
